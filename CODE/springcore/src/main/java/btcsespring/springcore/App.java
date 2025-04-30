c34abeb6dc1199b9943b3a014c643cdbfff5b2f5
Aditya Kumar <adityakumar2019.ak@gmail.com>
2025-04-30 19:30:46 +0530

    Add initial Spring Core project setup with Maven and configuration files
    - Created .project and .settings files for Eclipse IDE integration.
    - Added Maven POM file with Spring Core and Spring Context dependencies.
    - Implemented basic Spring configuration in XML for an Employee bean.
    - Developed Employee class with properties and printDetail method.
    - Created main application class to load Spring context and retrieve bean.
    - Added JUnit test class for basic application testing.
    - Included JDBC overview and prepared statement documentation in PDF format.
    - Added JSP documentation for server-side technology overview.
    - Included presentation files for Servlet communication and Spring Framework introduction.

---
+++ CODE/springcore/src/main/java/btcsespring/springcore/App.java
@@ -0,0 +1,22 @@
+package btcsespring.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context1 = new ClassPathXmlApplicationContext
+    			 ("btcsespring/springcore/21btcseconfig.xml");
+    	
+    	Employee e =(Employee)context1.getBean("emp1");
+    	
+    	e.printDetail();
+    	System.out.println( "Hello World!" );
+    }
+}
